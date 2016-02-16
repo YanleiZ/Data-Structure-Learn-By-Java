a56c9c81039c7251dcf47e019525849fe2344b16
Yanlei <yanlei.z.g@gmail.com>
2016-02-16 17:03:57 +0800

    完成串匹配类的编写

--- 数据结构学习/src/简单算法实现/Brute_Force.java
+++ 数据结构学习/src/简单算法实现/Brute_Force.java
@@ -2,11 +2,11 @@ package 简单算法实现;
 
 //Brute-Force算法实现字符串的匹配
 public class Brute_Force {
-	public static int bijiao(String s1, String s2) {
+	public static int chazhao(String s1, String s2) {
 		if (s1.length() > 0 && s2.length() > 0 && s1.length() >= s2.length()) {
 			int i = 0, j = 0;
 			while (i < s1.length()) {
-				if (s1.charAt(i) == s1.charAt(j)) {
+				if (s1.charAt(i) == s2.charAt(j)) {
 					i++;
 					j++;
 				} else {
@@ -14,15 +14,18 @@ public class Brute_Force {
 					j = 0;
 				}
 				if (j == s2.length()) {
-					return i - j;
+					return i-j;
 				}
 			}
 
+		}
+		else{
+			System.out.println("输入错误！");
 		}
 		return -1;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(bijiao("asdcsdfacxgs", "cx"));
+		System.out.println( chazhao("hbadsghbdcxxxxx", "cxxxxx"));
 	}
 }
