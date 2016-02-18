d9c872527ed2d6d5cf585911b59775be0ca0de82
Yanlei <yanlei.z.g@gmail.com>
2016-02-18 17:16:44 +0800

    改进了KMP算法的getNext方法2016年2月18日17:16:39

--- 数据结构学习/src/简单算法实现/KMP.java
+++ 数据结构学习/src/简单算法实现/KMP.java
@@ -35,11 +35,26 @@ public class KMP {
 		int j = 0, k = -1;
 		int[] next = new int[pattern.length()];
 		next[0] = -1;
+		// while (j < pattern.length() - 1) {
+		// if (k == -1 || pattern.charAt(j) == pattern.charAt(k)) {
+		// j++;
+		// k++;
+		// next[j] = k;
+		// } else {
+		// k = next[k];
+		// }
+		// }
+		// 改进KMP算法的getNext方法
 		while (j < pattern.length() - 1) {
 			if (k == -1 || pattern.charAt(j) == pattern.charAt(k)) {
 				j++;
 				k++;
-				next[j] = k;
+				if (pattern.charAt(j) != pattern.charAt(k)) {
+					next[j] = k;
+				} else {
+					next[j] = next[k];
+				}
+
 			} else {
 				k = next[k];
 			}
