10405bdbd5b318de6675ec39c19f949da9671d62
Yanlei <yanlei.z.g@gmail.com>
2016-02-26 10:30:15 +0800

    DigitTower类是使用递归实现数字塔的打印

--- 数据结构学习/src/简单算法实现/DigitTower.java
+++ 数据结构学习/src/简单算法实现/DigitTower.java
@@ -1,5 +1,6 @@
 package 简单算法实现;
 
+//递归实现打印数字塔
 public class DigitTower {
 	public static void line(int i, int n) {
 		System.out.print(String.format("%3d", i));
@@ -11,8 +12,8 @@ public class DigitTower {
 
 	public static void main(String args[]) {
 		int n = 9;
-		for (int i = 1; i < n; i++) {
-			System.out.println(String.format("%" + 3 * (n - i + 1) + "c", ' '));
+		for (int i = 1; i <= n; i++) {
+			System.out.print(String.format("%" + 3 * (n - i + 1) + "c", ' '));
 			line(1, i);
 			System.out.println();
 		}
